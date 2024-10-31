3df680cb0a8b8d64f279150cbec30d69990daee6
yeabsirashimelis <shimelisyeabsira123@gmail.com>
2024-10-31 13:46:41 -0700

    build error correction

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import NavBar from "@/components/NavBar";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "photoswipe/dist/photoswipe.css";
+import "leaflet/dist/leaflet.css";
 
 export const metadata = {
   title: "Ruphael School",
--- components/ContactUsMap.jsx
+++ components/ContactUsMap.jsx
@@ -1,51 +1,81 @@
 "use client";
 import Image from "next/image";
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import markerIcon from "leaflet/dist/images/marker-icon.png";
-import markerShadow from "leaflet/dist/images/marker-shadow.png";
-import markerRetina from "leaflet/dist/images/marker-icon-2x.png";
+// Dynamically import the map components with ssr disabled
+const MapContainer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.MapContainer),
+  { ssr: false }
+);
+const TileLayer = dynamic(
+  () => import("react-leaflet").then((mod) => mod.TileLayer),
+  { ssr: false }
+);
+const Marker = dynamic(
+  () => import("react-leaflet").then((mod) => mod.Marker),
+  { ssr: false }
+);
+const Popup = dynamic(() => import("react-leaflet").then((mod) => mod.Popup), {
+  ssr: false,
+});
 
 function ContactUsMap() {
-  const customIcon = new L.Icon({
-    iconUrl: markerIcon,
-    iconRetinaUrl: markerRetina,
-    shadowUrl: markerShadow,
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-    popupAnchor: [1, -34],
-    shadowSize: [41, 41],
-  });
+  const [isMounted, setIsMounted] = useState(false);
+  const [L, setL] = useState(null);
+
+  useEffect(() => {
+    import("leaflet").then((L) => {
+      setL(L.default);
+      setIsMounted(true);
+    });
+  }, []);
+
+  if (!isMounted) return null;
+
+  const customIcon =
+    L &&
+    new L.Icon({
+      iconUrl: "/marker-icon.png", // Move these to public directory
+      iconRetinaUrl: "/marker-icon-2x.png",
+      shadowUrl: "/marker-shadow.png",
+      iconSize: [25, 41],
+      iconAnchor: [12, 41],
+      popupAnchor: [1, -34],
+      shadowSize: [41, 41],
+    });
 
   return (
-    <MapContainer
-      center={[8.7484, 38.9783]}
-      zoom={12}
-      scrollWheelZoom={false}
-      className="mt-4 h-[330px] w-full"
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[8.7484, 38.9783]} icon={customIcon}>
-        <Popup>
-          <div>
-            <Image
-              width={0}
-              height={0}
-              sizes="100vw"
-              src="/schoolPhoto.jpg"
-              alt="School photo"
-              className=" h-[60px] w-full"
-            />
-            <p>schoolname school</p>
-          </div>
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div className="mt-4 h-[330px] w-full">
+      {isMounted && (
+        <MapContainer
+          center={[8.7484, 38.9783]}
+          zoom={12}
+          scrollWheelZoom={false}
+          className="h-full w-full"
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[8.7484, 38.9783]} icon={customIcon}>
+            <Popup>
+              <div>
+                <Image
+                  width={0}
+                  height={0}
+                  sizes="100vw"
+                  src="/schoolPhoto.jpg"
+                  alt="School photo"
+                  className="h-[60px] w-full"
+                />
+                <p>schoolname school</p>
+              </div>
+            </Popup>
+          </Marker>
+        </MapContainer>
+      )}
+    </div>
   );
 }
 
